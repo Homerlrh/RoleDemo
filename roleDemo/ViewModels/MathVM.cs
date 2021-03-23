fb149c16f2c54ab36417ae8f1c9f36555ef681bc
homer li <Homer.lee.0508@gmail.com>
2021-03-22 22:15:24 -0700

    init

---
+++ roleDemo/ViewModels/MathVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace roleDemo.ViewModels
+{
+    public class MathVM
+    {
+        public int num1 { get; set; }
+        public int num2 { get; set; }
+    }
+}
